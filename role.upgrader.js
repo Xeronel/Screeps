69b448ef31aafadac3ade3da34f96db147fa3e49
Xeronel <ripster1121@gmail.com>
2017-05-17 00:15:36 -0400

    Transfer all energy when upgrading

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,15 +2,24 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
-            var cSource = creep.pos.findClosestByPath(FIND_SOURCES);
-            if(creep.harvest(cSource) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(cSource, {visualizePathStyle: {stroke: '#ffffff'}});
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+        }
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+            creep.say('🚧 upgrade');
+        }
+
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#00ffff'}});
             }
         }
         else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#00ffff'}});
+            var cSource = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (creep.harvest(cSource) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(cSource, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 	},
