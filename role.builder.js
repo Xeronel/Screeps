f9c3f02d6a06aff513aa38d13ad08d2f10a45657
Xeronel <ripster1121@gmail.com>
2017-05-17 10:53:31 -0400

    Use upgrader as backup role for builder

--- role.builder.js
+++ role.builder.js
@@ -1,3 +1,5 @@
+var roleUpgrader = require('role.upgrader');
+
 var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -16,9 +18,10 @@ var roleBuilder = {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else {
+                roleUpgrader.run(creep);
             }
-	    }
-	    else {
+        } else {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
