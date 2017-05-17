f6505b9eec5772b41bc89ddfac1eced786d13a22
Xeronel <ripster1121@gmail.com>
2017-05-17 00:14:44 -0400

    Only transfer energy to the spawn if it is not at capacity

--- role.harvester.js
+++ role.harvester.js
@@ -3,10 +3,10 @@ var roleUpgrader = require('role.upgrader');
 var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+	    if(creep.carry.energy < creep.carryCapacity && Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
             var cSource = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(cSource) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(cSource, {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.moveTo(cSource, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
