a2f66033e0b9c5aa303a364eb67274de42e18ebd
Xeronel <ripster1121@gmail.com>
2017-05-17 17:29:26 -0400

    Builder find sources by closest path

--- role.builder.js
+++ role.builder.js
@@ -27,7 +27,7 @@ roleBuilder.run = function (creep) {
             roleUpgrader.run(creep);
         }
     } else {
-        var sources = creep.pos.findClosestByRange(FIND_SOURCES);
+        var sources = creep.pos.findClosestByPath(FIND_SOURCES);
         if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
             creep.moveTo(sources, {
                 visualizePathStyle: {
