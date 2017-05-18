2b6d18fd6f839979939f83d588963a327d5e5feb
Xeronel <ripster1121@gmail.com>
2017-05-17 21:46:47 -0400

    Remove pointless if statement

--- population.manager.js
+++ population.manager.js
@@ -6,20 +6,18 @@ var population = require('population');
 var populationManager = {};
 populationManager.spawnCreeps = function () {
     Object.getOwnPropertyNames(population).forEach(function (role) {
-        if (role !== 'spawnCreeps') {
-            var creepType = population[role];
-            var rolePopulation = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+        var creepType = population[role];
+        var rolePopulation = _.filter(Game.creeps, (creep) => creep.memory.role == role);
 
-            if (rolePopulation.length < creepType.qty) {
-                var newName = Game.spawns['Spawn1'].createCreep(
-                    creepType.role.parts,
-                    undefined, {
-                        role: role
-                    }
-                );
-                if (newName !== ERR_NOT_ENOUGH_RESOURCES && newName !== ERR_BUSY) {
-                    console.log('Spawned ' + role + ': ' + newName);
+        if (rolePopulation.length < creepType.qty) {
+            var newName = Game.spawns['Spawn1'].createCreep(
+                creepType.role.parts,
+                undefined, {
+                    role: role
                 }
+            );
+            if (newName !== ERR_NOT_ENOUGH_RESOURCES && newName !== ERR_BUSY) {
+                console.log('Spawned ' + role + ': ' + newName);
             }
         }
     });
