a45b7675e62a366adeafca27f4921ca26769a0a9
Xeronel <ripster1121@gmail.com>
2017-05-17 22:19:18 -0400

    Merge harmless

--- population.js
+++ population.js
@@ -3,49 +3,23 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 
-var population =
-{
-    'harvester':
-    {
+var population = {
+    'harvester': {
         qty: 3,
         role: roleHarvester
     },
-    'upgrader':
-    {
+    'upgrader': {
         qty: 2,
         role: roleUpgrader
     },
-    'builder':
-    {
+    'builder': {
         qty: 3,
         role: roleBuilder
     },
-    'repairer':
-    {
-        qty: 1,
+    'repairer': {
+        qty: 2,
         role: roleRepairer
     }
 };
-
-population.spawnCreeps = function () {
-    Object.getOwnPropertyNames(population).forEach(function (role) {
-        if (role !== 'spawnCreeps') {
-            var creepType = population[role];
-            var rolePopulation = _.filter(Game.creeps, (creep) => creep.memory.role == role);
-
-            if (rolePopulation.length < creepType.qty) {
-                var newName = Game.spawns['Spawn1'].createCreep(
-                    creepType.role.parts,
-                    undefined, {
-                        role: role
-                    }
-                );
-                if (newName !== ERR_NOT_ENOUGH_RESOURCES && newName !== ERR_BUSY) {
-                    console.log('Spawned ' + role + ': ' + newName);
-                }
-            }
-        }
-    });
-}
 
 module.exports = population;
