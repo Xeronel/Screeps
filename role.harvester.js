729f1bdb64c4e77e866270607ec1bb4ee973ccbc
Xeronel <ripster1121@gmail.com>
2017-05-21 18:37:23 -0400

    Only harvest from sources

--- role.harvester.js
+++ role.harvester.js
@@ -2,25 +2,20 @@ var roleUpgrader = require('role.upgrader');
 var Role = require('role.proto');
 
 var roleHarvester = new Role();
-roleHarvester.run = function(creep) {
+roleHarvester.run = function (creep) {
     var structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function(obj) {
+        filter: function (obj) {
             return obj.energy < obj.energyCapacity;
         }
     });
     if (creep.carry.energy < creep.carryCapacity && structure) {
-        var cSource = creep.pos.findClosestByPath(FIND_SOURCES) ||
-            creep.pos.findClosestByRange(FIND_SOURCES);
-        var ESource = creep.pos.findClosestByPath(FIND_SOURCES);
-
-        if (creep.pickup(cSource) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(cSource, {
-                visualizePathStyle: {
-                    stroke: '#ffaa00'
-                }
+        var source = creep.pos.findClosestByPath(FIND_SOURCES) ||
+            creep.pos.findClosestByPath(FIND_SOURCES, {
+                ignoreCreeps: true
             });
-        } else {
-            creep.harvest_move(ESource);
+
+        if (source) {
+            creep.harvest_move(source);
         }
     } else if (structure) {
         creep.transfer_move(structure);
