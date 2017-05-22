40a77417d35765fa5370fb90aa6d3ba9993d5d35
Xeronel <ripster1121@gmail.com>
2017-05-22 10:59:04 -0400

    Let harvesters use storage

--- role.harvester.js
+++ role.harvester.js
@@ -4,24 +4,28 @@ var Role = require('role.proto');
 var roleHarvester = new Role();
 roleHarvester.run = function (creep) {
     var structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function (obj) {
-            return obj.energy < obj.energyCapacity;
-        }
+            filter: (s) => {
+                if (s.structureType === STRUCTURE_STORAGE) {
+                    return s.store.energy < s.storeCapacity;
+                } else {
+                    return s.energy < s.energyCapacity;
+                }
+            }
     });
-    if (creep.carry.energy < creep.carryCapacity && structure) {
-        var source = creep.pos.findClosestByPath(FIND_SOURCES) ||
-            creep.pos.findClosestByPath(FIND_SOURCES, {
-                ignoreCreeps: true
-            });
+if (creep.carry.energy < creep.carryCapacity && structure) {
+    var source = creep.pos.findClosestByPath(FIND_SOURCES) ||
+        creep.pos.findClosestByPath(FIND_SOURCES, {
+            ignoreCreeps: true
+        });
 
-        if (source) {
-            creep.harvest_move(source);
-        }
-    } else if (structure) {
-        creep.transfer_move(structure);
-    } else {
-        roleUpgrader.run(creep);
+    if (source) {
+        creep.harvest_move(source);
     }
+} else if (structure) {
+    creep.transfer_move(structure);
+} else {
+    roleUpgrader.run(creep);
+}
 };
 
 module.exports = roleHarvester;
