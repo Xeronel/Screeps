e16b092fed475f6956b54663ed924152bf769ed1
Smogcheck <wicketfire@gmail.com>
2017-05-22 14:48:04 -0700

    resolved issue with ripster

--- role.harvester.js
+++ role.harvester.js
@@ -2,46 +2,29 @@ var roleUpgrader = require('role.upgrader');
 var Role = require('role.proto');
 
 var roleHarvester = new Role();
-roleHarvester.run = function (creep) {
+roleHarvester.run = function(creep) {
     var structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (s) => {
-                if (s.structureType === STRUCTURE_STORAGE) {
-                    return s.store.energy < s.storeCapacity;
-                } else {
-                    return s.energy < s.energyCapacity;
-                }
+        filter: (s) => {
+            if (s.structureType === STRUCTURE_STORAGE) {
+                return s.store.energy < s.storeCapacity;
+            } else {
+                return s.energy < s.energyCapacity;
             }
+        }
     });
-if (creep.carry.energy < creep.carryCapacity && structure) {
-    var source = creep.pos.findClosestByPath(FIND_SOURCES) ||
-        creep.pos.findClosestByPath(FIND_SOURCES, {
-            ignoreCreeps: true
-        });
-
-<<<<<<< HEAD
-        if (creep.pickup(cSource) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(cSource, {     
-                visualizePathStyle: {
-                    stroke: '#ffaa00'
-                }
+    if (creep.carry.energy < creep.carryCapacity && structure) {
+        var source = creep.pos.findClosestByPath(FIND_SOURCES) ||
+            creep.pos.findClosestByPath(FIND_SOURCES, {
+                ignoreCreeps: true
             });
-        } else {
-            creep.harvest_move(ESource);
+        if (source) {
+            creep.harvest_move(source);
         }
     } else if (structure) {
         creep.transfer_move(structure);
     } else {
         roleUpgrader.run(creep);
-=======
-    if (source) {
-        creep.harvest_move(source);
->>>>>>> ripster
     }
-} else if (structure) {
-    creep.transfer_move(structure);
-} else {
-    roleUpgrader.run(creep);
-}
 };
 
 module.exports = roleHarvester;
