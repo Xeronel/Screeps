38e82e4839287a11ef94a40627ed597249ef91f9
Smogcheck <wicketfire@gmail.com>
2017-05-22 18:26:53 -0700

    format fixes

--- role.repairer.js
+++ role.repairer.js
@@ -4,7 +4,7 @@ var $ = require('utils');
 
 var roleRepairer = new Role();
 roleRepairer.repairing = {}; // Store repair targets
-roleRepairer.run = function (creep) {
+roleRepairer.run = function(creep) {
     if (creep.memory.repairing && creep.carry.energy == 0) {
         creep.memory.repairing = false;
     }
