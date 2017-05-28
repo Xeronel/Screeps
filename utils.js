bbc5485eda95aedea8df9fde0b0d422f0da76703
Xeronel <ripster1121@gmail.com>
2017-05-28 13:23:21 -0400

    Undo breaking changes in utils

--- utils.js
+++ utils.js
@@ -1,16 +1,11 @@
 var wrapper = {};
 wrapper.each = function (fn) {
     var iter = (val, idx, array) => {
-        return fn(val, idx, array);
+        fn(val, idx, array);
     };
 
     if (Array.isArray(this.object)) {
-        for (var idx = 0; idx < this.object.length; idx++) {
-            var val = this.object[idx];
-            if (iter(val, idx, this.object) == false) {
-                break;
-            }
-        }
+        this.object.forEach(iter);
     } else {
         Object.getOwnPropertyNames(this.object).forEach(iter);
     }
