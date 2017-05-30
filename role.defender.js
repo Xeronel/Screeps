e5e18e64c4457cb8fc8b7da1ea31947d1973d62f
Xeronel <ripster1121@gmail.com>
2017-05-29 20:40:15 -0400

    Put energy in towers

--- role.defender.js
+++ role.defender.js
@@ -4,13 +4,26 @@ var logger = require('logger');
 
 var roleDefender = new Role();
 roleDefender.run = function run(creep) {
-    var log = logger.getLogger('RoleDefender');
+    var log = logger.getLogger('RoleDefender', logger.DEBUG);
     var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+    var tower = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+        filter: (s) => s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity
+    });
+
     if (enemy) {
+        // If an enemy exists, try to attack it
         if (creep.rangedAttack(enemy) == ERR_NOT_IN_RANGE) {
             creep.moveTo(enemy);
         }
+    } else if (tower) {
+        // If a tower exists try to fill it with energy
+        if (creep.carry.energy < creep.carryCapacity) {
+            creep.obtainClosestSource(creep.findClosestSource());
+        } else {
+            creep.transfer_move(tower);
+        }
     } else {
+        // Fall back to upgrader
         roleUpgrader.run(creep);
     }
 }
