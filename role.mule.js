898446b85adcabe5bcb15a0d4a3c39ea90b3eb81
Smogcheck <wicketfire@gmail.com>
2017-06-07 16:18:52 -0700

    added resource type to transfer move and fixed the mule to handle other resource types

--- role.mule.js
+++ role.mule.js
@@ -4,9 +4,14 @@ var logger = require('logger');
 
 var roleMule = new Role();
 
-function getAndTransfer(creep, target) {
-    if (_.sum(creep.carry) < creep.carryCapacity) {
+function getAndTransfer(creep, target, totalCarry) {
+    if (creep.carry.energy === 0) {
         creep.obtainClosestSource(creep.findClosestSource());
+    } else if (totalCarry - creep.carry.energy > 0) {
+        creep.transfer_move(creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (s) => s.structureType === STRUCTURE_CONTAINER ||
+                s.structureType === STRUCTURE_STORAGE
+        }));
     } else {
         creep.transfer_move(target);
     }
@@ -16,14 +21,15 @@ roleMule.run = function run(creep, spawn) {
     var log = logger.getLogger('RoleMule');
 
     var eStructure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-        filter: (s) => s.structureType === STRUCTURE_TOWER || s.structureType === STRUCTURE_EXTENSION && s.energy < s.energyCapacity
+        filter: (s) => s.structureType === STRUCTURE_TOWER ||
+            s.structureType === STRUCTURE_EXTENSION ||
+            s.structureType === STRUCTURE_SPAWN &&
+            s.energy < s.energyCapacity
     });
-
-    if (eStructure) {
+    var totalCarry = _.sum(creep.carry);
+    if (eStructure && totalCarry > 0) {
         // If a tower exists try to fill it with energy
-        getAndTransfer(creep, eStructure);
-    } else if (spawn.energy < spawn.energyCapacity) {
-        getAndTransfer(creep, spawn);
+        getAndTransfer(creep, eStructure, totalCarry);
     } else {
         // Fall back to upgrader
         roleUpgrader.run(creep);
