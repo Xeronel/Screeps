6aaa13fbbb78eb96a4ebe9ffe15abb209533514b
Smogcheck <wicketfire@gmail.com>
2017-06-07 21:13:38 -0700

    fuck you ripster

--- role.harvester.js
+++ role.harvester.js
@@ -3,7 +3,7 @@ var Role = require('role.proto');
 
 var roleHarvester = new Role();
 roleHarvester.run = function run(creep) {
-    var storage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+    var storage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
         filter: (s) => {
             if (s.structureType === STRUCTURE_STORAGE) {
                 if (s.store.energy < s.storeCapacity) {
@@ -18,9 +18,6 @@ roleHarvester.run = function run(creep) {
             }
         }
     });
-    var structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (s) => s.structureType === STRUCTURE_EXTENSION && s.energy < s.energyCapacity
-    });
 
     // If creep isn't carrying max capacity, harvest
     if (creep.totalCarry < creep.carryCapacity) {
@@ -33,8 +30,6 @@ roleHarvester.run = function run(creep) {
         if (source) {
             creep.harvest_move(source);
         }
-    } else if (structure) {
-        creep.transfer_move(structure);
     } else if (storage) {
         creep.transfer_move(storage);
     } else {
