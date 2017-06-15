d16db873568a87d613e7c6a3acd8f90e00a8547e
Xeronel <ripster1121@gmail.com>
2017-06-15 13:19:57 -0400

    Add onDeath event

--- memory.manager.js
+++ memory.manager.js
@@ -1,11 +1,20 @@
-var memManager = {
-    cleanCreeps: function cleanCreeps() {
-        for (var name in Memory.creeps) {
-            if (!Game.creeps[name]) {
+var logger = require('logger');
+var population = require('population');
+
+var memManager = {};
+memManager.cleanCreeps = function cleanCreeps() {
+    var log = logger.getLogger('MemMan', logger.DEBUG);
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            if (population.hasOwnProperty(Memory.creeps[name].role)) {
+                population[Memory.creeps[name].role].role.onDeath(name, Memory.creeps[name]);
+            }
+            if (Memory.creeps[name] !== undefined) {
+                log.debug(`Deleted ${name} from memory.`);
                 delete Memory.creeps[name];
             }
         }
     }
-}
+};
 
 module.exports = memManager;
--- role.proto.js
+++ role.proto.js
@@ -1,7 +1,7 @@
 function Role() {}
-Role.prototype._run = function run() {};
+Role.prototype._run = function () {};
 Role.prototype.run = Object.defineProperty(Role.prototype, 'run', {
-    get: function run() {
+    get: function () {
         return function run() {
             // If a creep was passed as the first argument
             if (arguments.length > 0 && arguments[0] instanceof Creep) {
@@ -20,7 +20,23 @@ Role.prototype.run = Object.defineProperty(Role.prototype, 'run', {
         this._run = fn;
     }
 });
+Role.prototype._onSpawn = function () {};
+Role.prototype.onSpawn = Object.defineProperty(Role.prototype, 'onSpawn', {
+    get: function get () {
+        return function () {
+            if (arguments.length > 0 && arguments[0] instanceof Creep) {
+                arguments[0].memory.id = arguments[0].id;
+            }
+            this._onSpawn.apply(this, arguments);
+        };
+    },
+    set: function (fn) {
+        this._onSpawn = fn;
+    }
+});
+
 Role.prototype.onSpawn = function onSpawn(creep) {};
+Role.prototype.onDeath = function onDeath(name, memory) {};
 Role.prototype.icon = '';
 
 module.exports = Role;
