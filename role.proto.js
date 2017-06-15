e39e404564d4ab79deb0f2891c405fb5d1d9b9f5
Xeronel <ripster1121@gmail.com>
2017-06-15 12:11:09 -0400

    Add onSpawn event

--- role.proto.js
+++ role.proto.js
@@ -1,9 +1,26 @@
-function Role() {
-    this.run = function () {};
-}
-
-Role.prototype.run = function run(creep) {};
+function Role() {}
+Role.prototype._run = function run() {};
+Role.prototype.run = Object.defineProperty(Role.prototype, 'run', {
+    get: function run() {
+        return function run() {
+            // If a creep was passed as the first argument
+            if (arguments.length > 0 && arguments[0] instanceof Creep) {
+                var creep = arguments[0];
+                if (creep.spawning && !creep.memory.wasSpawning) {
+                    creep.memory.wasSpawning = true;
+                } else if (!creep.spawning && creep.memory.wasSpawning) {
+                    delete creep.memory.wasSpawning;
+                    this.onSpawn(creep);
+                }
+            }
+            this._run.apply(this, arguments)
+        }
+    },
+    set: function set(fn) {
+        this._run = fn;
+    }
+});
+Role.prototype.onSpawn = function onSpawn(creep) {};
 Role.prototype.icon = '';
-/** @param {Creep} creep **/
 
 module.exports = Role;
