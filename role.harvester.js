9f9267b899ad402d1853908d71b6fa51e99ce29d
Xeronel <ripster1121@gmail.com>
2017-06-16 21:01:19 -0400

    Give harvesters dedicated sources

--- role.harvester.js
+++ role.harvester.js
@@ -1,7 +1,38 @@
 var roleUpgrader = require('role.upgrader');
 var Role = require('role.proto');
+var logger = require('logger');
+var $ = require('utils');
 
 var roleHarvester = new Role();
+roleHarvester.log = logger.getLogger('RoleHarvest');
+
+roleHarvester.onSpawn = function onSpawn(creep) {
+    if (Memory.sources === undefined) {
+        Memory.sources = {};
+    }
+    // Get an untargeted source
+    var source = creep.pos.findClosestByPath(FIND_SOURCES, {
+        ignoreCreeps: true,
+        filter: (s) => Memory.sources[s.id] === undefined || Memory.sources[s.id] < $(s).countHarvestable()
+    });
+    creep.memory.source = source.id;
+    if (Memory.sources[source.id] === undefined) {
+        this.log.debug(`${creep.name} created ${source.id}`);
+        Memory.sources[source.id] = 1;
+    } else {
+        Memory.sources[source.id]++;
+        this.log.debug(`${creep.name} incremented ${source.id} to ${Memory.sources[source.id]}`);
+    }
+};
+
+roleHarvester.onDeath = function onDeath(name) {
+    var sourceId = Memory.creeps[name].source;
+    if (Memory.sources && Memory.sources[sourceId]) {
+        Memory.sources[sourceId]--;
+        this.log.debug(`${name} decremented ${sourceId} ${Memory.sources[sourceId]}`);
+    }
+};
+
 roleHarvester.run = function run(creep) {
     if (creep.carry.energy == 0) {
         creep.memory.collecting = true;
@@ -12,15 +43,8 @@ roleHarvester.run = function run(creep) {
 
     // If creep isn't carrying max capacity, harvest
     if (creep.memory.collecting) {
-        var source = creep.pos.findClosestByPath(FIND_SOURCES, {
-                filter: (s) => s.energy > 0
-            }) ||
-            creep.pos.findClosestByPath(FIND_SOURCES, {
-                ignoreCreeps: true
-            });
-        if (source) {
-            creep.harvest_move(source);
-        }
+        source = Game.getObjectById(creep.memory.source);
+        creep.harvest_move(source);
     } else {
         var storage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
             filter: (s) => {
@@ -44,6 +68,7 @@ roleHarvester.run = function run(creep) {
         }
     }
 };
+
 roleHarvester.icon = "⛏";
 
 module.exports = roleHarvester;
